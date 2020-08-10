e904b7a344a1b448f88f47eed3062ae5c2c20d3a
Tanmoydasgupta <joydasgupta97@gmail.com>
2020-08-10 11:55:45 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,12 @@
+// falsy: false,0,"",undefined,null,NaN
+// truthy: '0', '' ,[].
+
+let name = 2;
+if(name || name == 0){
+     console.log("Condition in true");
+        }
+else{
+    console.log("Condition is false");
+}        
+
+
