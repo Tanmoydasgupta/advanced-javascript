70a7da00071998d8e19bd506ed8832355786f44c
Tanmoydasgupta <joydasgupta97@gmail.com>
2020-08-11 20:00:47 +0600

    nullvsundeined

---
+++ null-undefined.js
@@ -0,0 +1,7 @@
+function add(num1,num2){
+    console.log(num1,num2);
+}
+const result =add(13);
+console.log(result);
+let fun = undefined;  // undefined is  a reserved word (not to use as a variable name)
+console.log(fun);
